5267d105bf399fb9e9903768bb60f1a8cea2e8ff
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2018-12-22 01:26:03 +0530

    Front end display and validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ApplicationRecord
+	validates :short_name, presence: true, length: {minimum: 2, maximum: 50}
+	validates :name, presence: true, length: {minimum: 5, maximum: 50}
+	validates :description, presence: true, length: {minimum: 10, maximum: 300}
 
 end
