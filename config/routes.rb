abaecdab3c8f1c73ccc3776c55d10061a2510b50
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2018-12-23 12:18:14 +0530

    added login routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
 	get 'about', to: 'pages#about'
 	get 'courses/new', to: 'courses#new'
 	resources :students, except: [:destroy]
+	get 'login', to: 'logins#new'
+	post 'login', to: 'logins#create'
+	delete 'logout', to: 'logins#destroy'
 
 end
