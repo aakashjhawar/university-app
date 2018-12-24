770d69a5db797e98819cc3a6ac27f6c22b57fbac
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2018-12-24 13:23:40 +0530

    Add associations from front end

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 	get 'login', to: 'logins#new'
 	post 'login', to: 'logins#create'
 	delete 'logout', to: 'logins#destroy'
+	post 'course_enroll', to: 'student_courses#create'
 
 end
