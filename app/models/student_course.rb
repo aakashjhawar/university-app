67f3118e25bb0dacd039e05f10f25196856b2e94
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2018-12-24 12:12:00 +0530

    create student_course model

---
+++ app/models/student_course.rb
@@ -0,0 +1,5 @@
+class StudentCourse < ApplicationRecord
+	belongs_to :student
+	belongs_to :course
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_22_072052) do
+ActiveRecord::Schema.define(version: 2018_12_24_055244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2018_12_22_072052) do
     t.string "short_name"
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "student_courses", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
