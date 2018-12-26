050a0467688654f1bf59298614ae9bdde8d21440
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2018-12-26 22:29:36 +0530

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root 'courses#index'
+	root 'courses#home'
 	get 'courses', to: 'courses#index'
 	get 'about', to: 'pages#about'
 	get 'courses/new', to: 'courses#new'
